43ab37bee51270f55039c100660b296c642c8248
Lucas Cirillo Berardi <lucascirilloberardi@HP-ProBook-440-G8-Notebook-PC-c0332be8.localdomain>
2024-10-18 10:19:40 +0000

    feat(index.js): do exercise 2 and 3

--- index.js
+++ index.js
@@ -43,22 +43,69 @@ console.log(sum(1)(a));
 // La funcion debe retornar true si un usuario se logueo en los ultimos 30 minutos
 
 const users = [
-    {
-      id: "1",
-      login_time: new Date("2024-10-18T08:55:51.01"),
-    },
-    {
-      id: "2",
-      login_time: new Date("2024-10-18T09:30:51.01"),
-    },
-    {
-      id: "3",
-      login_time: new Date("2024-04-20T09:40:51.01"),
-    },
+  {
+    id: "1",
+    login_time: new Date("2024-10-18T08:55:51.01"),
+  },
+  {
+    id: "2",
+    login_time: new Date("2024-10-18T08:30:51.01"),
+  },
+  {
+    id: "3",
+    login_time: new Date("2024-10-18T09:40:51.01"),
+  },
 ];
 
-function loginLastThirty(user){
-    
+function loginLastThirty(arrayOfUsers, num) {
+  const now = new Date();
+  const comparationNum = num;
+
+  return arrayOfUsers.some((user) => now - user.login_time <= comparationNum);
+}
+console.log(loginLastThirty(users, 30 * 60 * 1000));
+
+// https://stackoverflow.com/questions/10599148/how-do-i-get-the-current-time-only-in-javascript
+// Busque en esta pagina para ver bien lo del Date porque no entendia su funcionamiento
+
+// 3) Escribir una funcion que determine si 2 strings son un anagrama
+// Ignorar espacios en los strings
+// No diferenciar entre mayusculas y minusculas
+// Retornar true si es anagrama y false de lo contrario
+
+function isAnagram(str1, str2) {
+  const withoutSpaces = (str) => str.split(' ').join('').toLowerCase();
+
+  const string1 = withoutSpaces(str1);
+  const string2 = withoutSpaces(str2);
+
+  if (string1.length !== string2.length) {
+    console.log("There are not anagrams");
+    return false;
+  }
+
+  const count = {};
+
+  for (const character of string2) {
+    count[character] = (count[character] || 0) + 1;
+  }
+
+  for (const character of string2) {
+    if (!count[character]) {
+      return false;
+    }
+    count[character]--;
+  }
+
+  return true;
 }
 
-loginLastThirty(users[0])
+console.log(isAnagram("  DOrMYtoRy", "dirty room  "));
+
+
+// 4) Escribir una funcion generadora de una contraseña segura. Debe recibir un numero y generar una contraseña de esa longitud que cumpla con las siguientes consignas:
+// Al menos una mayuscula
+// Al menos una minuscula
+// Al menos un numero
+// Al menos un caracter especial
+// Debe retornar la contraseña generada
